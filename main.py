4f56a87b8d5e7e0e8714b77fa7c0d8a2428effc4
Danny0223 <danny_90033@yahoo.com>
2019-08-21 12:22:46 -0700

    added

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-
-ssssssssssssssssssssssssssssssssssimport webapp2
+import webapp2
 import jinja2
 import os
 
@@ -13,78 +12,84 @@ class Meal(object):
     def __init__(self, _name, _ingredientList):
         self.name = _name
         self.ingredientList = _ingredientList
-        var_dict=
-            [
+        var_dict=[
                 {
                     name:"Burger"
                         [
-                            ingred1:"bun"
-                            ingred2:"onion"
-                            ingred3:"patty"
-                            ingred4:"lettuce"
-                            ingred5:"tomatoe"
+                            ingred1:"bun",
+                            ingred2:"onion",
+                            ingred3:"patty",
+                            ingred4:"lettuce",
+                            ingred5:"tomatoe",
                         ],
                     name:"Taco"
                         [
-                            ingred1:"tortilla"
-                            ingred2:"meat"
+                            ingred1:"tortilla",
+                            ingred2:"meat",
                         ],
                     name:"Burrito"
                         [
-                            ingred1:"tortilla"
-                            ingred2:"beans"
-                            ingred3:"meat"
-                            ingred4:"rice"
-                            ingred5:"cheese"
-                            ingred6:"lettuce"
-                            ingred7:"sourCream"
+                            ingred1:"tortilla",
+                            ingred2:"beans",
+                            ingred3:"meat",
+                            ingred4:"rice",
+                            ingred5:"cheese",
+                            ingred6:"lettuce",
+                            ingred7:"sourCream",
                         ],
                     name:"Chicken Alfredo"
                         [
-                            ingred1:"boneless chicken"
-                            ingred2:"fettucine"
-                            ingred3:"garlic"
-                            ingred4:"sauce"
+                            ingred1:"boneless chicken",
+                            ingred2:"fettucine",
+                            ingred3:"garlic",
+                            ingred4:"sauce",
                         ],
                     name:"Sandwich"
                         [
-                            ingred1:"bread"
-                            ingred2:"turkey"
-                            ingred3:"cheese"
-                            ingred4:"tomatoe"
-                            ingred5:"mayo"
-                            ingred6:"lettuce"
+                            ingred1:"bread",
+                            ingred2:"turkey",
+                            ingred3:"cheese",
+                            ingred4:"tomatoe",
+                            ingred5:"mayo",
+                            ingred6:"lettuce",
                         ],
                     name:"Salad"
                         [
-                            ingred1:"lettuce"
-                            ingred2:"spinage"
-                            ingred3:"tomatoe"
-                            ingred4:"cruton"
-                            ingred5:"carrot"
+                            ingred1:"lettuce",
+                            ingred2:"spinage",
+                            ingred3:"tomatoe",
+                            ingred4:"cruton",
+                            ingred5:"carrot",
                         ],
                     name:"Meatloaf"
                         [
-                            ingred1:"groundbeef"
-                            ingred2:"bread crumbs"
-                            ingred3:"onion"
-                            ingred4:"milk"
-                            ingred5:"egg"
-                            ingred6:"ketchup"
-                            ingred7:"sauce"
-                            ingred8:"parsley"
-                            ingred9:"garlic powder"
-                            ingred10:"salt"
-                            ingred11:"pepper"
-                        ]
+                            ingred1:"groundbeef",
+                            ingred2:"bread crumbs",
+                            ingred3:"onion",
+                            ingred4:"milk",
+                            ingred5:"egg",
+                            ingred6:"ketchup",
+                            ingred7:"sauce",
+                            ingred8:"parsley",
+                            ingred9:"garlic powder",
+                            ingred10:"salt",
+                            ingred11:"pepper",
+                        ],
                 }
+    def post(self):
+        userMeal = self.request.get("meme_choice")
 
+        var_dict = {
+
+            "meme_img_url": memeStringToUrl(userMeme)
+    }
+        results_template = the_jinja_env.get_template('templates/results.html')
+        self.response.write(results_template.render(var_dict))
 
 
 
             ]
 m1 = Meal("Burger",)
-"""
 class EnterInfoHandler(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
@@ -96,10 +101,10 @@ class FoodInfoHandler(webapp2.RequestHandler):
         results_template = the_jinja_env.get_template('templates/Burgers.html')
         self.response.write(results_template.render())
 
-"""
+
 
 
 app = webapp2.WSGIApplication([
     ('/',EnterInfoHandler ),
-    ('/danny',FoodInfoHandler)
+    ('/Food',FoodInfoHandler)
 ], debug=True)
