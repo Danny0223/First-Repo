ae73db133cf7b4a8125234edf36d9f8a16a0c3b2
USER <grod82918@gmail.com>
2019-08-21 12:29:43 -0700

    add to main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-ssssssssssssssssssssssssssssssssssimport webapp2
+import webapp2
 import jinja2
 import os
 
@@ -18,64 +18,136 @@ class Meal(object):
                 {
                     name:"Burger"
                         [
-                            ingred1:"bun"
-                            ingred2:"onion"
-                            ingred3:"patty"
-                            ingred4:"lettuce"
+                            ingred1:"bun",
+                            ingred2:"onion",
+                            ingred3:"patty",
+                            ingred4:"lettuce",
                             ingred5:"tomatoe"
                         ],
                     name:"Taco"
                         [
-                            ingred1:"tortilla"
+                            ingred1:"tortilla",
                             ingred2:"meat"
                         ],
                     name:"Burrito"
                         [
-                            ingred1:"tortilla"
-                            ingred2:"beans"
-                            ingred3:"meat"
-                            ingred4:"rice"
-                            ingred5:"cheese"
-                            ingred6:"lettuce"
+                            ingred1:"tortilla",
+                            ingred2:"beans",
+                            ingred3:"meat",
+                            ingred4:"rice",
+                            ingred5:"cheese",
+                            ingred6:"lettuce",
                             ingred7:"sourCream"
                         ],
                     name:"Chicken Alfredo"
                         [
-                            ingred1:"boneless chicken"
-                            ingred2:"fettucine"
-                            ingred3:"garlic"
+                            ingred1:"boneless chicken",
+                            ingred2:"fettucine",
+                            ingred3:"garlic",
                             ingred4:"sauce"
                         ],
                     name:"Sandwich"
                         [
-                            ingred1:"bread"
-                            ingred2:"turkey"
-                            ingred3:"cheese"
-                            ingred4:"tomatoe"
-                            ingred5:"mayo"
+                            ingred1:"bread",
+                            ingred2:"turkey",
+                            ingred3:"cheese",
+                            ingred4:"tomatoe",
+                            ingred5:"mayo",
                             ingred6:"lettuce"
                         ],
                     name:"Salad"
                         [
-                            ingred1:"lettuce"
-                            ingred2:"spinage"
-                            ingred3:"tomatoe"
-                            ingred4:"cruton"
+                            ingred1:"lettuce",
+                            ingred2:"spinage",
+                            ingred3:"tomatoe",
+                            ingred4:"cruton",
                             ingred5:"carrot"
                         ],
                     name:"Meatloaf"
                         [
-                            ingred1:"groundbeef"
-                            ingred2:"bread crumbs"
-                            ingred3:"onion"
-                            ingred4:"milk"
-                            ingred5:"egg"
-                            ingred6:"ketchup"
-                            ingred7:"sauce"
-                            ingred8:"parsley"
-                            ingred9:"garlic powder"
-                            ingred10:"salt"
+                            ingred1:"groundbeef",
+                            ingred2:"bread crumbs",
+                            ingred3:"onion",
+                            ingred4:"milk",
+                            ingred5:"egg",
+                            ingred6:"ketchup",
+                            ingred7:"sauce",
+                            ingred8:"parsley",
+                            ingred9:"garlic powder",
+                            ingred10:"salt",
                             ingred11:"pepper"
+                        ]
+                    name:"Enchiladas"
+                        [
+                            ingred1:"groundbeef",
+                            ingred2:"tortilla",
+                            ingred3:"onion",
+                            ingred4:"sauce",
+                            ingred5:"cheese",
+                            ingred6:"green chiles"
+                        ]
+                    name:"Stew"
+                        [
+                            ingred1:"beef",
+                            ingred2:"onion",
+                            ingred3:"carrot",
+                            ingred4:"garlic",
+                            ingred5:"tamato",
+                            ingred6:"peas",
+                            ingred7:"sauce",
+                            ingred8:"potatoes",
+                            ingred9:"veggie broth",
+                            ingred10:"salt",
+                            ingred11:"pepper"
+                        ]
+                    name:"Pizza"
+                        [
+                            ingred1:"dough",
+                            ingred2:"tomato sauce",
+                            ingred3:"cheese",
+                            ingred4:"pepperoine",
+                        ]
+                    name:"Dumplings"
+                        [
+                            ingred1:"flour",
+                            ingred2:"baking powder",
+                            ingred3:"salt",
+                            ingred4:"milk",
+                            ingred5:"butter"
+                        ]
+                    name:"Casserole"
+                        [
+                            ingred1:"red peppers"
+                            ingred2:"tomato sauce"
+                            ingred3:"zucchini"
+                            ingred4:"squash"
+                            ingred5:"mushrooms"
+                            ingred6:"pasta"
+                        ]
+                    name:"Chile"
+                        [
+                            ingred1:"groundbeef"
+                            ingred2:"tomato sauce"
+                            ingred3:"cheese"
+                            ingred4:"green pepper"
+                            ingred5:"beans"
+                            ingred6:"garlic"
+                        ]
+                    name:"MacNCheese"
+                        [
+                            ingred1:"Pasta"
+                            ingred2:"breed crumbs"
+                            ingred3:"cheese"
+                            ingred4:"cheese"
+                            ingred4:"milk"
+                        ]
+                    name:"Hot dog"
+                        [
+                            ingred1:"bread"
+                            ingred2:"sausage"
+                            ingred3:"cheese"
+                            ingred4:"ketchup"
+                            ingred4:"mustard"
                         ]
                 }
 
