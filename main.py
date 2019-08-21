acd86de337ffe9e79e150dc6dc2e12558e50e76a
Danny0223 <danny_90033@yahoo.com>
2019-08-21 14:57:47 -0700

    nmain

--- main.py
+++ main.py
@@ -2,55 +2,58 @@ import webapp2
 import jinja2
 import os
 import models
-from models import BlogPost.
+from models import BlogPost
 
 the_jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+#MAIN PAGE WHERE USER IS FIRST AT
+
 class MainPageHandler(webapp2.RequestHandler):
     def get(self):
         result_template = the_jinja_env.get_template('templates/index.html')
         self.response.write(result_template.render())
 
-class FamilyPageHandler(webapp2.RequestHandler):
+class ResultPageHandler(webapp2.RequestHandler):
     def get(self):
-        result_template = the_jinja_env.get_template('templates/about_my_family.html')
+        result_template = the_jinja_env.get_template('templates/results.html')
         self.response.write(result_template.render())
 
+"""
 class BlogHandler(webapp2.RequestHandler):
     def get(self):
         result_template = the_jinja_env.get_template('templates/new_post.html')
         self.response.write(result_template.render())
-
+"""
 
 
 class ViewHandler(webapp2.RequestHandler):
     def get(self):
-        result_template = the_jinja_env.get_template('templates/view_all_posts.html')
+        result_template = the_jinja_env.get_template('templates/view_all_meals.html')
         self.response.write(result_template.render())
 
     def post(self):
-        TitleName = self.request.get("TName")
-        ContentName = self.request.get("CName")
-        AuthorName = self.request.get("AName")
+        Choose Your Meal = self.request.get("Meal")
+        #ContentName = self.request.get("CName")
+        #AuthorName = self.request.get("AName")
 
         var_dict = {
-            "TName": TitleName,
-            "CName": ContentName,
-            "AName": AuthorName
+            "Meal": Choose Your Meal,
+            #"CName": ContentName,
+            #"AName": AuthorName
         }
-        blog1 = BlogPost(Name = AuthorName,title= TitleName,content= ContentName)
-        blog1.put()
+        meal1= Food_Meal(name = Choose Your Meal)
+        meal1.put()
 
-        result_template = the_jinja_env.get_template('templates/view_all_posts.html')
+        result_template = the_jinja_env.get_template('templates/view_all_meals.html')
         self.response.write(result_template.render(var_dict))
 
 app = webapp2.WSGIApplication([
     ('/', MainPageHandler),
-    ('/about_my_family', FamilyPageHandler),
-    ('/blog',BlogHandler),
-    ('/view_all_posts',ViewHandler)
+    ('/Results', ResultsPageHandler),
+    #('/blog',BlogHandler),
+    ('/view_all_meals',ViewHandler)
 
 ], debug=True)
