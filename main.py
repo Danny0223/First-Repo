95ab96d9271ad3b7c8dc5ac5fb1d75336f338e63
Danny0223 <danny_90033@yahoo.com>
2019-08-21 11:41:48 -0700

    main

--- main.py
+++ main.py
@@ -8,6 +8,82 @@ the_jinja_env = jinja2.Environment(
     autoescape=True
 )
 
+class Meal(object):
+    def __init__(self, _name, _ingredientList):
+        self.name = _name
+        self.ingredientList = _ingredientList
+        var_dict=
+            [
+                {
+                    name:"Burger"
+                        [
+                            ingred1:"bun"
+                            ingred2:"onion"
+                            ingred3:"patty"
+                            ingred4:"lettuce"
+                            ingred5:"tomatoe"
+                        ],
+                    name:"Taco"
+                        [
+                            ingred1:"tortilla"
+                            ingred2:"meat"
+                        ],
+                    name:"Burrito"
+                        [
+                            ingred1:"tortilla"
+                            ingred2:"beans"
+                            ingred3:"meat"
+                            ingred4:"rice"
+                            ingred5:"cheese"
+                            ingred6:"lettuce"
+                            ingred7:"sourCream"
+                        ],
+                    name:"Chicken Alfredo"
+                        [
+                            ingred1:"boneless chicken"
+                            ingred2:"fettucine"
+                            ingred3:"garlic"
+                            ingred4:"sauce"
+                        ],
+                    name:"Sandwich"
+                        [
+                            ingred1:"bread"
+                            ingred2:"turkey"
+                            ingred3:"cheese"
+                            ingred4:"tomatoe"
+                            ingred5:"mayo"
+                            ingred6:"lettuce"
+                        ],
+                    name:"Salad"
+                        [
+                            ingred1:"lettuce"
+                            ingred2:"spinage"
+                            ingred3:"tomatoe"
+                            ingred4:"cruton"
+                            ingred5:"carrot"
+                        ],
+                    name:"Meatloaf"
+                        [
+                            ingred1:"groundbeef"
+                            ingred2:"bread crumbs"
+                            ingred3:"onion"
+                            ingred4:"milk"
+                            ingred5:"egg"
+                            ingred6:"ketchup"
+                            ingred7:"sauce"
+                            ingred8:"parsley"
+                            ingred9:"garlic powder"
+                            ingred10:"salt"
+                            ingred11:"pepper"
+                        ]
+                }
+
+
+
+
+            ]
+m1 = Meal("Burger",)
+"""
 class EnterInfoHandler(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
@@ -16,9 +92,10 @@ class EnterInfoHandler(webapp2.RequestHandler):
 
 class FoodInfoHandler(webapp2.RequestHandler):
     def get(self):
+        results_template = the_jinja_env.get_template('templates/Burgers.html')
+        self.response.write(results_template.render())
 
-
-
+"""
 
 
 app = webapp2.WSGIApplication([
