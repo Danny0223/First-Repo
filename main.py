9f1f3c27aad533c855a6ac70b1e50a78256e4694
Danny0223 <danny_90033@yahoo.com>
2019-08-22 15:18:35 -0700

    main

--- main.py
+++ main.py
@@ -11,12 +11,36 @@ the_jinja_env = jinja2.Environment(
 
 def mealStringToUrl(meal_choice):
     imgUrl = ""
+    ingredients = ""
     if meal_choice == "burger":
         imgUrl = "http://img.thedailybeast.com/image/upload/v1493130772/galleries/2010/03/24/the-40-deadliest-fast-food-meals/fast-food---in-out-cheeseburger_bw2cot.jpg"
+        ingredients = 
     elif meal_choice == "taco":
-        imgUrl = "https://media1.tenor.com/images/cfeb7a77e287d674d56d4706dcaeab1c/tenor.gif?itemid=5446149"
+        imgUrl = "https://img1.cookinglight.timeinc.net/sites/default/files/styles/medium_2x/public/1519665233/slow-cooker-carnitas-tacos-ck-1804p38.jpg?itok=Kew1JNyq"
     elif meal_choice == "pasta":
-        imgUrl = "https://66.media.tumblr.com/c6547a1ce571b9432cfba47b5fac538f/tumblr_pn06z5RAz51wt43j1_540.jpg"
+        imgUrl = "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2015/9/15/1/FNK_Chicken-Fettucine-Alfredo_s4x3.jpg.rend.hgtvcom.826.620.suffix/1442375396342.jpeg"
+    elif meal_choice == "sandwich":
+        imgUrl = "https://indianakitchen.com/wp-content/uploads/2015/03/Ham-Sandwich.jpg"
+    elif meal_choice == "salad":
+        imgUrl = "https://www.tasteofhome.com/wp-content/uploads/2017/10/exps6498_MRR133247D07_30_5b_WEB-2.jpg"
+    elif meal_choice == "meatloaf":
+        imgUrl = "https://images-gmi-pmc.edge-generalmills.com/3e0ded09-f8a2-45b6-aff7-e08ab138ed84.jpg"
+    elif meal_choice == "enchiladas":
+        imgUrl = "https://www.chilipeppermadness.com/wp-content/uploads/2019/02/Beef-Enchiladas-Recipe1.jpg"
+    elif meal_choice == "stew":
+        imgUrl = "https://www.simplyrecipes.com/wp-content/uploads/2015/03/irish-beef-stew-horiz-a2-1800.jpg"
+    elif meal_choice == "pizza":
+        imgUrl = "https://peopledotcom.files.wordpress.com/2019/02/gettyimages-938742222.jpg"
+    elif meal_choice == "dumplings":
+        imgUrl = "https://images-gmi-pmc.edge-generalmills.com/6059b986-4800-4508-8546-40cb56e3d815.jpg"
+    elif meal_choice == "casserole":
+        imgUrl = "https://images-gmi-pmc.edge-generalmills.com/fe6e9467-247e-4b3d-9694-973a818eeab1.jpg"
+    elif meal_choice == "chile":
+        imgUrl = "https://cdn-image.foodandwine.com/sites/default/files/styles/medium_2x/public/2012-r-xl-four-chile-chili.jpg?itok=_NWkr1bj"
+    elif meal_choice == "macNcheese":
+        imgUrl = "https://www.tasteofhome.com/wp-content/uploads/2018/04/exps36313_TH1115229D39A.jpg"
+    elif meal_choice == "hotdog":
+        imgUrl = "https://a1.olivecache.net/assets/components/phpthumbof/cache/all_american_hot_dogs_small.bfce0e026efbc76dc3fab481a14cef02.jpg"
     else:
         imgUrl = ""
     return imgUrl
@@ -53,6 +77,7 @@ class ResultsPageHandler(webapp2.RequestHandler):
         var_dict = {
             "meal_choice_dict_key": mealChoiceFromForm,
             "meal_pic_dict_key":mealPictureFromFunction
+
         }
 
         result_template = the_jinja_env.get_template('templates/results.html')
