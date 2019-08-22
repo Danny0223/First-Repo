107490884e8a4c1f0ec04e23dc63fb2153d3ad71
spiral342 <54331191+spiral342@users.noreply.github.com>
2019-08-22 12:12:54 -0700

    model.py update
    this isnt scrapping, not gonna work

--- models.py
+++ models.py
@@ -3,7 +3,6 @@ from google.appengine.ext import ndb
 class Food_Meal(ndb.Model):
     name = ndb.StringProperty(required=True)
     ingredientList = ndb.StringProperty(required=True)
-
     # Burger = Food_Meal(
     #     name = 'userMeal',
     #     ingredientList=[
