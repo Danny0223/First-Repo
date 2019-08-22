d4a352298389ef6887a1c6a7d3a6460e91785b08
Danny0223 <danny_90033@yahoo.com>
2019-08-22 10:10:54 -0700

    models

--- models.py
+++ models.py
@@ -3,23 +3,76 @@ from google.appengine.ext import ndb
 class Food_Meal(ndb.Model):
     name = ndb.StringProperty(required=True)
     ingredientList = ndb.StringProperty(required=True)
-"""
+
     Burger = Food_Meal(
         name = 'userMeal',
         ingredientList=[
-            ingredient1="bun"
-            ingredient2="onion"
-
+            ingred1="bun"
+            ingred2="onion"
+            ingred3="patty"
+            ingred4="lettuce"
+            ingred5="tomatoe"
         ]
     )
-"""
-"""
-    Burger = Food_Meal(
+
+    Tacos = Food_Meal(
         name = 'userMeal',
         ingredientList=[
-            ingredient1="bun"
-            ingredient2="onion"
+            ingred1="tortilla"
+            ingred2="meat"
+        ]
+    )
+
+    Pasta = Food_Meal(
+        name = 'userMeal',
+        ingredientList=[
+            ingred1=""
+            ingred2=""
 
         ]
     )
-"""
+
+     = Food_Meal(
+        name = 'userMeal',
+        ingredientList=[
+            ingred1=""
+            ingredd2=""
+
+        ]
+    )
+
+     = Food_Meal(
+        name = 'userMeal',
+        ingredientList=[
+            ingred1=""
+            ingredd2=""
+
+        ]
+    )
+
+     = Food_Meal(
+        name = 'userMeal',
+        ingredientList=[
+            ingred1=""
+            ingredd2=""
+
+        ]
+    )
+
+     = Food_Meal(
+        name = 'userMeal',
+        ingredientList=[
+            ingred1=""
+            ingredd2=""
+
+        ]
+    )
+
+     = Food_Meal(
+        name = 'userMeal',
+        ingredientList=[
+            ingred1=""
+            ingredd2=""
+
+        ]
+    )
