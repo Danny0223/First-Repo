67bec0d3d390960adb6fb27e8b3f546852d75ee0
Danny0223 <danny_90033@yahoo.com>
2019-08-23 11:02:40 -0700

    THIS IS PROGRESS

--- main.py
+++ main.py
@@ -43,6 +43,75 @@ def mealStringToUrl(meal_choice):
         imgUrl = ""
     return imgUrl
 
+def mealCalories(meal_choice):
+    calories = ""
+    if meal_choice == "burger":
+        calories = "596"
+    elif meal_choice == "tacos":
+        calories = "492"
+    elif meal_choice == "pasta":
+        calories = "587"
+    elif meal_choice == "sandwich":
+        calories = "203"
+    elif meal_choice == "salad":
+        calories = "183"
+    elif meal_choice == "meatloaf":
+        calories = "809"
+    elif meal_choice == "enchiladas":
+        calories = "857"
+    elif meal_choice == "stew":
+        calories = "448"
+    elif meal_choice == "pizza":
+        calories = "222"
+    elif meal_choice == "dumplings":
+        calories = "333"
+    elif meal_choice == "casserole":
+        calories = "444"
+    elif meal_choice == "chile":
+        calories = "555"
+    elif meal_choice == "macNcheese":
+        calories = "666"
+    elif meal_choice == "hotdog":
+        calories = "777"
+    else:
+        calories = ""
+    return calories
+
+def mealIngred(meal_choice):
+    ingred = ""
+    if meal_choice == "burger":
+        ingred = "<h1>bun:133<br>onion:32<br>groundbeef:336<br>lettuce:10<br>tomato:22<br>cheese:63</h1>"
+    elif meal_choice == "tacos":
+        ingred = "<h1>tortilla:112<br>groundbeef:336<br>salse:10<br>onion:32<br>cilantro:22</h1>"
+    elif meal_choice == "pasta":
+        ingred = "<h1>noodles:200<br>tomatosauce:29<br>tomato:22<br>groundbeef:336</h1>"
+    elif meal_choice == "sandwich":
+        ingred = "<h1>turnkey:45<br>lettuce:10<br>tamato:22<br>bread:69<br>mayo:57</h1>"
+    elif meal_choice == "salad":
+        ingred = "<h1>lettuce:75<br>spinach:7<br>choppoedtamto:10<br>croutons:31<br>ranch:60</h1>"
+    elif meal_choice == "meatloaf":
+        ingred = "<h1>groundbeef:336<br>groundbeef:336<br>ketcup:15<br>ketchup:15<br>onion:32<br>breadcrumbs:107</h1>"
+    elif meal_choice == "enchiladas":
+        ingred = "<h1>tortilla:112<br>tortilla:112<br>tortilla:112<br>groundbeef:336<br>cheese:120<br>redsauce:65</h1>"
+    elif meal_choice == "stew":
+        ingred = "<h1>beef:256<br>potatoe:67<br>beefstock:31<br>carrot:27<br>peas:67</h1>"
+    elif meal_choice == "pizza":
+        ingred = "<h1>dough:160<br>cheese:340<br>pepperoni:200<br>tomatosauce:29</h1>"
+    elif meal_choice == "dumplings":
+        ingred = "<h1>flour:104<br>milk:149<br>butter:150<br>sugar:17</h1>"
+    elif meal_choice == "casserole":
+        ingred = "<h1>noodles:209<br>tomatosauce:29<br>mozzarella:348<br>breadcrumbs:107<br>groundbeef:336</h1>"
+    elif meal_choice == "chili":
+        ingred = "<h1>groundbeef:336<br>onion:32<br>greenpepper:38<br>tomatosauce:29<br>beans:112</h1>"
+    elif meal_choice == "macNcheese":
+        ingred = "<h1>noodles:209<br>mozzarella:348<br>breadcrumbs:107<br>bacon:43</h1>"
+    elif meal_choice == "hotdog":
+        ingred = "<h1>sausage:209<br>cheese:63<br>sausageBun:144<br>mustard<br>mustard=8<br>ketchup:16</h1>"
+    else:
+        ingred = ""
+    return ingred
+
+
 #MAIN PAGE WHERE USER IS FIRST AT
 
 class MainPageHandler(webapp2.RequestHandler):
@@ -69,17 +138,19 @@ class ResultsPageHandler(webapp2.RequestHandler):
 
         mealPictureFromFunction = mealStringToUrl(mealChoiceFromForm)
 
+        mealCaloriesFromFunction = mealCalories(mealChoiceFromForm)
 
-
-
+        mealIngredFromFunction = mealIngred(mealChoiceFromForm)
 
         var_dict = {
             "meal_choice_dict_key": mealChoiceFromForm,
-            "meal_pic_dict_key":mealPictureFromFunction
+            "meal_pic_dict_key":mealPictureFromFunction,
+            "meal_calories_dict_key": mealCaloriesFromFunction
         }
 
         result_template = the_jinja_env.get_template('templates/results.html')
         self.response.write(result_template.render(var_dict))
+        self.response.write(mealIngredFromFunction)
 
 """
 class BlogHandler(webapp2.RequestHandler):
